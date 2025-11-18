d774b483fdfdfe98a5fd06cbb1c13b81c8e9cc48
bihaikenbaptiste16-ui <bihaikenbaptiste16@gmail.com>
2025-11-18 16:14:08 -0500

    App 1

--- app/src/main/java/com/example/sheriffcom/MainActivity.kt
+++ app/src/main/java/com/example/sheriffcom/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.sheriffcom
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,7 +11,34 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContentView(R.layout.activity_main)
+        findViewById<View>(R.id.Cover).setOnClickListener {
+            findViewById<View>(R.id.Cover).visibility = View.GONE
+            findViewById<View>(R.id.Question).visibility = View.VISIBLE
+        }
+
+
+        findViewById<View>(R.id.Question).setOnClickListener {
+            findViewById<View>(R.id.Question).visibility = View.GONE
+            findViewById<View>(R.id.Answer).visibility = View.VISIBLE
+        }
+
+        findViewById<View>(R.id.Answer).setOnClickListener {
+            findViewById<View>(R.id.Question2).visibility = View.VISIBLE
+            findViewById<View>(R.id.Answer).visibility = View.GONE
+        }
+        findViewById<View>(R.id.Question2).setOnClickListener {
+            findViewById<View>(R.id.Question2).visibility = View.GONE
+            findViewById<View>(R.id.Answer2).visibility = View.VISIBLE
+        }
+        findViewById<View>(R.id.Answer2).setOnClickListener {
+            findViewById<View>(R.id.Answer2).visibility = View.GONE
+            findViewById<View>(R.id.Question).visibility = View.VISIBLE
+        }
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
